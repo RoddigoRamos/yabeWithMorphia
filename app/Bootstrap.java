f5aa7a2aa1e8aaacc3b2c1f73b5707377728bed9
RoddrigoRamos <rodrigoramosnajera@gmail.com>
2015-11-20 22:28:26 -0700

    Configurando MongoLab.

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -7,11 +7,11 @@ import models.*;
 @OnApplicationStart
 public class Bootstrap extends Job {
 
-    public void doJob() {
-        // Check if the database is empty
-        if(User.count() == 0) {
-            Fixtures.loadModels("initial-data.yml");
-        }
-    }
+//    public void doJob() {
+//        // Check if the database is empty
+//        if(User.count() == 0) {
+//            Fixtures.loadModels("initial-data.yml");
+//        }
+//    }
 
 }
